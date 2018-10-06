b392dea904f486e668e6771cfab1841afcb2a924
Mitchell Hodzen <mitchellhodzen@yahoo.com>
2018-10-06 13:12:23 -0400

    Changed implementation of message manager to use a queue

--- KECS/include/MessageManager.h
+++ KECS/include/MessageManager.h
@@ -1,28 +1,35 @@
 #pragma once
 #include <type_traits>
 #include <iostream>
+#include <queue>
 #include <vector>
-
 class MessageManager
 {
 public:
-	//Template function to get the current vector of the message
-	template<typename Message> static std::vector<Message> GetMessageVector()
+	template<typename Message> static void PushMessage(Message message)
 	{
-		return messageVector<Message>;
+		messageQueue<Message>.push(message);
 	}
 
-	template<typename Message> static void AddMessage(Message message)
+	template<typename Message> static Message PopMessage()
 	{
-		messageVector<Message>.push_back(message);
+		Message message = messageQueue<Message>.top();
+		messageQueue<Message>.pop();
+		return message;
 	}
 
 	template<typename Message> static void ClearMessages()
 	{
-		messageVector<Message>.clear();
+		std::queue<Message, std::vector<Message>>().swap(messageQueue<Message>);
+	}
+
+	template<typename Message> static bool NotEmpty()
+	{
+		return !messageQueue<Message>.empty();
 	}
 private:
 	MessageManager() {};
 	~MessageManager() {};
-	template<typename T> static inline std::vector<T> messageVector;
+	//template<typename T> static inline std::vector<T> messageVector;
+	template<typename T> static inline std::queue<T, std::vector<T>> messageQueue  = std::queue<T, std::vector<T>>();
 };
