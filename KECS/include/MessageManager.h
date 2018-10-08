44788e5d1923345ed507480cf6bf53fb3e48db05
Mitchell Hodzen <mitchellhodzen@yahoo.com>
2018-10-07 20:12:10 -0400

    Fixed message manager

--- KECS/include/MessageManager.h
+++ KECS/include/MessageManager.h
@@ -6,30 +6,33 @@
 class MessageManager
 {
 public:
+	
 	template<typename Message> static void PushMessage(Message message)
 	{
-		messageQueue<Message>.push(message);
+		messageQueue<Message>->push(message);
 	}
 
 	template<typename Message> static Message PopMessage()
 	{
-		Message message = messageQueue<Message>.top();
-		messageQueue<Message>.pop();
+		Message message = messageQueue<Message>->front();
+		messageQueue<Message>->pop();
 		return message;
 	}
 
 	template<typename Message> static void ClearMessages()
 	{
-		std::queue<Message, std::vector<Message>>().swap(messageQueue<Message>);
+		//std::queue<Message, std::vector<Message>>().swap(messageQueue<Message>);
 	}
 
 	template<typename Message> static bool NotEmpty()
 	{
-		return !messageQueue<Message>.empty();
+		return !messageQueue<Message>->empty();
 	}
+	
 private:
 	MessageManager() {};
 	~MessageManager() {};
+	//static inline std::queue<int, std::vector<int>> messageQueue;
+	template<typename T> static inline std::queue<T>* messageQueue = new std::queue<T>;
 	//template<typename T> static inline std::vector<T> messageVector;
-	template<typename T> static inline std::queue<T, std::vector<T>> messageQueue  = std::queue<T, std::vector<T>>();
 };
